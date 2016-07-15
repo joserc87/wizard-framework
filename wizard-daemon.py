65e3d9151398716825a4b304627ac4c4f60804a9
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2016-07-15 11:36:49 +0200

    Add the initial version of the scripts

---
+++ wizard-daemon.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+import time
+from wizard import Broker, FileSystem
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class MyHandler(FileSystemEventHandler):
+  """
+  Watches for changes in the filesystem
+  When a wizard configuration or an event template has been changed, it uploads
+  it to the server
+  """
+
+  def __init__(self, broker, filesystem, debug=False):
+    """
+    Constructor
+    Args:
+      broker (Broker): object to communicate with the server
+      filesystem (FileSystem): helper object to access the filesystem
+    """
+    self.debug = debug
+    self.broker = broker
+    self.filesystem = filesystem
+
+  def log(self, msg):
+    if self.debug:
+      print msg
+
+  # File System events
+
+  def on_modified(self, event):
+    if event.is_directory:
+      self.log('something happened in directory {0} '.format(event.src_path))
+    else:
+      self.process_file(event.src_path)
+
+  def on_created(self, event):
+    if event.is_directory:
+      self.log('new directory {0} '.format(event.src_path))
+    else:
+      self.process_file(event.src_path)
+
+  def process_file(self, path):
+    parts = path.split('/')
+    try:
+      # parts = wizards/{id}/file.xml
+      if len(parts) >= 3 and parts[-3] == 'wizards':
+        wizardFolder = parts[-2].split(' - ', 1)
+        wizardID = int(wizardFolder[0])
+        fileparts = parts[-1].split('.')
+        # Get name and extension (file = name.ext)
+        if len(fileparts) == 2:
+          filename = fileparts[0]
+          extension = fileparts[1]
+          if filename == 'WizardConfiguration' and extension == 'xml':
+            # Send
+            self.update_wizard_configuration(wizardID, path)
+          elif filename == 'EventTemplate' and extension == 'xml':
+            self.update_event_template(wizardID, path)
+          else:
+            self.log('Unrecognized file {0} with extension {1}'.format(filename, extension))
+          return True
+        else:
+          self.log('File {0} has multiple dots?'.format(parts[-1]))
+      else:
+        self.log('Directory not deep enough or unexpected directory name {0}'.format(parts))
+    except ValueError:
+      self.log('value error')
+      return
+    self.log('Not doing anything for {0}'.format(path))
+
+  def update_wizard_configuration(self, wizardID, path):
+    wizard = broker.get_wizard(wizardID)
+    # Check that wizard still exists
+    if wizard is not None:
+      wizard.set_configuration(self.filesystem.read(path))
+      print "Uploading configuration xml for wizard {0}".format(wizard.name)
+    else:
+      print 'The wizard with ID {0} does not exist anymore!'.format(wizardID)
+
+  def update_event_template(self, wizardID, path):
+    wizard = broker.get_wizard(wizardID)
+    # Check that wizard still exists
+    if wizard is not None:
+      wizard.set_event_template(self.filesystem.read(path))
+      print "Uploading event template xml for wizard {0}".format(wizard.name)
+    else:
+      print 'The wizard with ID {0} does not exist anymore!'.format(wizardID)
+
+def login(broker):
+  import getpass
+  # Login
+  authenticated = ''
+  while authenticated != 'OK':
+    user = raw_input('User:')
+    pwd = getpass.getpass('Password:')
+    print('Authenticating...')
+    authenticated = broker.login(user, pwd)
+    print(authenticated)
+
+if __name__ == "__main__":
+  broker = Broker('http://WIN-DEVSRVR2012/')
+  fs = FileSystem()
+
+  # Login
+  login(broker)
+
+  # One time synchronization between the filesystem and the server
+  print 'SYNCHRONIZING...'
+  fs.update_wizard_folders(broker.get_wizards())
+  print 'DONE'
+
+  # Start the daemon
+  event_handler = MyHandler(broker, fs, debug=False)
+
+  observer = Observer()
+  observer.schedule(event_handler, path='.', recursive=True)
+  observer.start()
+
+  print 'READY'
+  print 'WATCHING FS'
+
+  try:
+    while True:
+      time.sleep(1)
+  except KeyboardInterrupt:
+    observer.stop()
+  observer.join()
---
+++ wizard.py
@@ -0,0 +1,207 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+class Wizard:
+
+  def __init__(self, broker, id, name, description, is_active):
+    self.broker = broker
+    self.id = id
+    self.name = name
+    self.description = description
+    self.is_active = is_active
+    self.configuration = None
+    self.event_template = None
+
+  def __str__(self):
+    return "Wizard[{0}]: '{1}' ({2})".format(self.id, self.name, 'active' if self.is_active else 'inactive')
+
+  def get_configuration(self):
+    if self.configuration is None: 
+      r = self.broker.get('/wizards/{0}/configuration'.format(self.id))
+      self.configuration = r.content
+    return self.configuration
+
+  def set_configuration(self, content):
+    if self.configuration != content:
+      r = self.broker.put('/wizards/{0}/configuration'.format(self.id), content)
+      self.configuration = content
+      if r.status_code == 200:
+        return True
+      else:
+        return False
+    else:
+      return True
+
+  def get_event_template(self):
+    if self.event_template is None: 
+      r = self.broker.get('/wizards/{0}/event'.format(self.id))
+      self.event_template = r.content
+    return self.event_template
+
+  def set_event_template(self, content):
+    if self.event_template != content:
+      r = self.broker.put('/wizards/{0}/event'.format(self.id), content)
+      self.event_template = content
+      if r.status_code == 200:
+        return True
+      else:
+        return False
+    else:
+      return True
+
+class Broker:
+
+  def __init__(self, host='http://localhost/', base='TCFDocumentWizardMobileWebService/TCFDocumentWizardAPI.svc'):
+    self.host = host
+    self.base = base
+    self.DEBUG = False
+  
+  def debug(self, msg):
+    if (self.DEBUG):
+      print(msg)
+
+  # part should start with '/'
+  def getURL(self, part):
+    return self.host + self.base + part
+
+  def get(self, part):
+    url = self.getURL(part)
+    self.debug(' -- GET :: {0}'.format(url))
+    return requests.get(url, auth=self.auth)
+
+  def put(self, part, content):
+    url = self.getURL(part)
+    self.debug(' -- PUT :: {0}'.format(url))
+    return requests.put(url, data=content, auth=self.auth)
+
+  # Web Service calls
+
+  def login(self, user, password):
+    self.auth = (user, password)
+    # Dummy call to see if we are authenticated
+    r = self.get('/work')
+    if r.status_code == 200:
+      return 'OK'
+    elif r.status_code == 401:
+      return 'Error 401: Unauthorized'
+    else:
+      return 'Error code: {0}'.format(r.status_code)
+    return 'OK'
+
+  def get_wizards(self):
+    # Call the rest web service
+    r = self.get('/wizards') 
+
+    # Check the output
+    if r.status_code == 200:
+      result = r.json()
+      if result['Error'] is None:
+        wizards = [ Wizard(
+          self,
+          int(w['ID']),
+          w['Name'],
+          w['Description'],
+          bool(w['IsActive'])) for w in result['Wizards'] ]
+
+        return wizards
+      else: # Result contains an error
+        print 'There was an error in the call to /wizards:'
+        print result['Error']
+    else:
+      print 'Something went wrong'
+    return None
+
+  def get_wizard(self, wizardID):
+    # Call the rest web service
+    r = self.get('/wizards/{0}'.format(wizardID)) 
+
+    # Check the output
+    if r.status_code == 200:
+      result = r.json()
+      if result['Error'] is None:
+        w = result['Wizard']
+        wizard = Wizard(
+          self,
+          int(w['ID']),
+          w['Name'],
+          w['Description'],
+          bool(w['IsActive']))
+
+        return wizard
+      else: # Result contains an error
+        print 'There was an error in the call to /wizards/{id}:'
+        print result['Error']
+    else:
+      print 'Something went wrong'
+    return None
+
+class FileSystem:
+
+  def __init__(self, path = './wizards/'):
+    self.path = path
+
+  def update_wizard_folders(self, wizards):
+    for wizard in wizards:
+      wizard_dir = '{0}{1} - {2}'.format(self.path,  wizard.id, wizard.name)
+      # If the folder does not exist, create it
+      if not os.path.isdir(wizard_dir):
+        print "New wizard found: '{0}'".format(wizard.name)
+        os.makedirs(wizard_dir)
+      files = [
+          (wizard_dir + '/WizardConfiguration.xml', wizard.get_configuration()),
+          (wizard_dir + '/EventTemplate.xml', wizard.get_event_template())]
+      for (filePath, content) in files:
+        # If the files do not exist, create them
+        if not os.path.exists(filePath):
+          print "Downloading file '{0}' for wizard '{1}'".format(filePath, wizard.name)
+          self.write(content, filePath)
+        else:
+          if not self.compare_file_with_data(filePath, content):
+            # Ask the user if he wants to overwrite
+            q = ''
+            while q != 'y' and q != 'n':
+              q = raw_input('The file {0} has been modified locally. Overrwite? (y/n)'.format(filePath)).lower()
+              if q.lower() == 'y':
+                self.write(content, filePath)
+              elif q.lower() == 'n':
+                print 'Ignoring. File not in sync'
+              else:
+                print 'Invalid option'
+
+  def compare_file_with_data(self, path, content):
+    # Return false when they are different
+    if self.read(path) == content:
+      return True
+    return False
+
+  def write(self, content, path):
+    with open(path, 'wb') as f:
+      f.write(content)
+
+  def read(self, path):
+    with open(path, 'rb') as f:
+      return f.read()
+
+
+# Main
+
+if __name__ == "__main__":
+  import getpass
+  broker = Broker('http://WIN-DEVSRVR2012/')
+
+  # Login
+  authenticated = ''
+  while authenticated != 'OK':
+    user = raw_input('User:')
+    pwd = getpass.getpass('Password:')
+    print('Authenticating...')
+    authenticated = broker.login(user, pwd)
+    print(authenticated)
+
+  # Get all the wizards
+  wizards = broker.get_wizards()
+
+  # Update the file system
+  fs = FileSystem()
+  fs.update_wizard_folders(wizards)
